879bb36254d902026d0ced9e280eb87925eb3150
mcarletti <marco.carletti@univr.it>
2017-11-03 12:18:07 +0100

    Update extensions to remove

--- clean.sh
+++ clean.sh
@@ -2,8 +2,9 @@
 
 echo 'Removing python cache, checkpoint files and results...'
 
-rm -rf __pycache__
-rm -f *.ckpt
-rm -f *.pth
+rm -f __pycache__/*.pyc # compiled python code
+rm -f __pycache__/*.pyo # optimized python code
+rm -f *.ckpt # checkpoint file(s)
+rm -f *.pth # result file(s)
 
 echo 'DONE'
