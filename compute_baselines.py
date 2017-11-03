Fix baseline computation

compute_baselines.py
@@ -3,10 +3,13 @@
 
 
 from __future__ import print_function
+
 import torch
-import torchvision.transforms as transforms
+from torchvision import transforms
 from torch.autograd import Variable
+
 import dataset
+
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -28,12 +31,24 @@ else:
     torch.cuda.manual_seed(seed)
     gpu_id = 0
     
+task = 'gender-from-depth' # age-from-faces, gender-from-depth, fat-from-depth
 dsname = 'HeadLegArmLess' # Full, HeadLess, HeadLegLess, HeadLegArmLess
-features_to_extract = 'vgg16' # hog, pca, vgg16
-regressor_mode = 'random_forest' # svm, random_forest
+features_to_extract = 'pca' # hog, pca, vgg16
+regressor_mode = 'svm' # svm, random_forest
+shuffle_train_set = True
 
-feature_filename = 'baseline_features_' + features_to_extract + '.pth'
-regressor_filename = 'baseline_regressor_' + regressor_mode + '_using_' + features_to_extract + '.pth'
+baseline_folder = 'baseline_results/' + task + '/'
+if not os.path.exists(baseline_folder):
+    os.makedirs(baseline_folder)
+
+feature_filename = baseline_folder + 'features_' + features_to_extract + '.pth'
+regressor_filename = baseline_folder + 'regressor_' + regressor_mode + '_using_' + features_to_extract + '.pth'
+
+print('******************')
+print('Task:', task)
+print('Feature:', features_to_extract)
+print('Regression:', regressor_mode)
+print('******************')
 
 # load extracted features
 if os.path.exists(feature_filename):
@@ -41,17 +56,30 @@ if os.path.exists(feature_filename):
     train_set, test_set = torch.load(feature_filename)
     
 else:
+
+    print('Loading dataset...')
     
     # Load dataset.
     nb_channels = 1 if features_to_extract in ['hog', 'pca'] else 3
     transf = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,) * nb_channels, (0.5,) * nb_channels)])
-    train_set = dataset.FATSYNTH(dsname, nb_channels, train=True, transform=transf)
-    test_set = dataset.FATSYNTH(dsname, nb_channels, train=False, transform=transf)
-    
+
+    if task == 'age-from-faces':
+        train_set = dataset.FACES(True, transf)
+        test_set = dataset.FACES(False, transf)
+
+    if task == 'gender-from-depth':
+        train_set = dataset.FATSYNTH(dsname, nb_channels, train=True, transform=transf)
+        test_set = dataset.FATSYNTH(dsname, nb_channels, train=False, transform=transf)
+
+    if task == 'fat-from-depth':
+        train_set = dataset.FATDATA(dsname, nb_channels, train=True, transform=transf)
+        test_set = dataset.FATDATA(dsname, nb_channels, train=False, transform=transf)
+
     batch_size = 1 if features_to_extract in ['hog', 'pca'] else 32
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False)
-    
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=shuffle_train_set, num_workers=0)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=0)
+
+
     # Extract features from datset.
     def extract_features(data_loader, network=None):
     
@@ -59,20 +87,27 @@ else:
         features = torch.FloatTensor()
         targets = torch.FloatTensor()
         
-        for i, data in enumerate(data_loader, network=None):
+        for i, data in enumerate(data_loader):
             
             x, y = data
-            x = x.squeeze().numpy()
-            y = y.squeeze()
 
             if features_to_extract == 'hog':
                 from skimage.feature import hog
-                from skimage import data
+                from skimage import data, color
+                x = x.squeeze().numpy()
+                y = y.squeeze()
+                if len(x.shape) == 3:
+                    x = np.transpose(x, (1, 2, 0))
+                x = color.rgb2gray(x)
                 batch_features = hog(x, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1))
                 batch_features = torch.FloatTensor(batch_features)
                 batch_features = batch_features.view(1, batch_features.numel())
 
             if features_to_extract == 'pca':
+                x = x.squeeze().numpy()
+                y = y.squeeze()
+                if len(x.shape) == 3:
+                    x = np.transpose(x, (1, 2, 0))
                 batch_features = torch.FloatTensor(x.reshape(1, x.size))
             
             if features_to_extract == 'vgg16':
@@ -91,16 +126,8 @@ else:
                 features = torch.cat((features, batch_features))
                 targets = torch.cat((targets, y))
             
-            print('[%3d/%3d]' % (i + 1, max_steps))
-        
-        if features_to_extract == 'pca':
-            from sklearn.decomposition import IncrementalPCA
-            print('Reducing feature dimension using Incremental PCA...')
-            new_feat_size = 1000
-            ipca = IncrementalPCA(n_components=new_feat_size, batch_size=new_feat_size)
-            ipca.fit(train_set[0].numpy())
-            train_set[0] = torch.FloatTensor(ipca.transform(train_set[0].numpy()))
-            test_set[0] = torch.FloatTensor(ipca.transform(test_set[0].numpy()))
+            if i % 100 == 0:
+                print('[%4d/%4d]' % (i + 1, max_steps))
     
         return features, targets
     
@@ -117,8 +144,22 @@ else:
     test_set = extract_features(test_loader, vgg16)
     
     # Save features.
+    print('Saving features...')
     torch.save((train_set, test_set), feature_filename)
-    
+
+    if features_to_extract == 'pca':
+        from sklearn.decomposition import IncrementalPCA
+        print('Reducing feature dimension using Incremental PCA...')
+        new_feat_size = 100
+        ipca = IncrementalPCA(n_components=new_feat_size, batch_size=new_feat_size)
+        ipca.fit(train_set[0].numpy())
+        new_train_data = torch.FloatTensor(ipca.transform(train_set[0].numpy()))
+        train_set = (new_train_data, train_set[1])
+        new_test_data = torch.FloatTensor(ipca.transform(test_set[0].numpy()))
+        test_set = (new_test_data, test_set[1])
+        print('Saving PCA features...')
+        torch.save((train_set, test_set), feature_filename)
+
 
 #%% ---------------------------------------------------------------------------
 # Compute baselines.