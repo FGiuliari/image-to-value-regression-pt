63b4450393f66f6021a7147c4301e45a988e2759
mcarletti <marco.carletti@univr.it>
2017-11-07 13:25:02 +0100

    Compute saliency maps

--- compute_saliency_maps.py
+++ compute_saliency_maps.py
@@ -1,7 +1,13 @@
+#!/usr/bin/python3
+
+
+#%% ---------------------------------------------------------------------------
+# Imports.
+
+
 from __future__ import print_function
 
 import torch
-import torchvision
 from torchvision import transforms
 from torch.autograd import Variable
 
@@ -11,6 +17,10 @@ import dataset
 
 import os
 import numpy as np
+
+
+#%% ---------------------------------------------------------------------------
+# Settings.
 
 
 # set the seed for debugging purposes
@@ -30,14 +40,20 @@ else:
 print('Loading data...')
 
 nb_channels = 1
-target_shape = (224, 224)
-target_shape = (nb_channels,) + target_shape
-batch_size = 1 # <== reduce this value if you encounter memory errors
+target_shape = (nb_channels,) + (224, 224)
 
 # the images are normalized between 0 and 1 (thanks to the ToTensor transformation) and then normalized between -1 and +1.
 transf = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,) * nb_channels, (0.5,) * nb_channels)])
 test_set = dataset.FATDATA('HeadLegArmLess', nb_channels, train=False, transform=transf)
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=0)
+
+# sort test set according to the ground truth value
+idx = np.argsort(test_set.test_values)
+test_set.test_values = test_set.test_values[idx]
+test_set.test_data = test_set.test_data[idx]
+
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=1, shuffle=False, num_workers=0)
+
+nb_samples = len(test_loader)
 
 model_filename = 'network_state_dict.ckpt'
 assert os.path.exists(model_filename)
@@ -45,46 +61,77 @@ assert os.path.exists(model_filename)
 print('Loading model file', model_filename)
 net = Net(target_shape, vgg16_basemodel=False, batch_normalization=True, dropout=False)
 net.load_state_dict(torch.load(model_filename))
+net.eval()
 
 if HAS_CUDA:
     net.cuda(gpu_id)
 
 
-def generate_occluded_imgs(image, wnd_size):
-    nb_images = image.shape[0] * image.shape[1]
-    images = np.zeros((nb_images,) + image.shape, dtype=np.float32)
-    coords = np.zeros((nb_images, 2), dtype=np.uint32)
-    counter = 0
-    h_size = int(wnd_size / 2)
-    for u in range(h_size, image.shape[0]-h_size):
-        for v in range(h_size, image.shape[1]-h_size):
-            tmp = np.array(image, copy=True)
-            tmp[u-h_size:u+h_size, v-h_size:v+h_size] = 0  # mask color
-            images[counter] = tmp.astype(np.float32)
-            coords[counter] = (u, v)
-            counter += 1
-    images = images[:counter]
-    coords = coords[:counter]
-    return np.squeeze(images), coords
+#%% ---------------------------------------------------------------------------
+# Generate saliency maps.
 
 
-SMAP = np.zeros((len(test_loader),) + (224, 224), dtype=np.float32)
+def generate_occluded_images(image, k_size, step=1):
+    ch, rows, cols = image.shape
+    nb_pixels = rows * cols
+    masked_images = torch.zeros(nb_pixels, ch, rows, cols)
+    coords = np.zeros((nb_pixels, 2), dtype=np.uint32)
+    it = 0
+    h_size = int(k_size / 2)
+    for u in range(h_size, rows - h_size, step):
+        for v in range(h_size, cols - h_size, step):
+            masked_images[it] = image.clone()
+            masked_images[it][:, u-h_size:u+h_size, v-h_size:v+h_size] = 0  # mask color
+            coords[it] = (u, v)
+            it += 1
+    masked_images = masked_images[:it]
+    coords = coords[:it]
+    return masked_images, coords
+
+
+saliency_maps = np.zeros((nb_samples,) + target_shape[1:], dtype=np.float32)
 for i, data in enumerate(test_loader):
-    print('Computing GT')
-    images, targets = data
+    print('[%3d/%3d] %.2f %%' % (i + 1, nb_samples, (i + 1) / nb_samples))
+    image, target = data
     if HAS_CUDA:
-        img = images.cuda(gpu_id)
-    gt = net(Variable(img))
-    gt = gt.data.cpu().squeeze().numpy()[0]
-    print('Generating masks...')
-    masked, coords = generate_occluded_imgs(images.squeeze().numpy(), 7)
-    print('Computing saliency')
-    for j in range(masked.shape[0]):
-        #img = transf(np.expand_dims(masked[j], 2))
-        img = torch.FloatTensor(np.expand_dims(masked[j], 0))
+        image = image.cuda(gpu_id)
+    ref_pred = net(Variable(image)).cpu().data[0][0]
+    masked_images, coords = generate_occluded_images(image[0], k_size=15, step=5)
+    nb_masked_images = masked_images.shape[0]
+    for j in range(0, nb_masked_images):
+        img = masked_images[j].unsqueeze(0)
         if HAS_CUDA:
             img = img.cuda(gpu_id)
-        yy = net(Variable(img.unsqueeze(0)))
-        WERj = yy.data.cpu().squeeze().numpy()[0] - test_set.test_values[i]
-        SMAP[i, coords[j][0], coords[j][1]] = WERj
-    break
+        pred = net(Variable(img)).cpu().data[0][0]
+        WERj = pred - test_set.test_values[i]
+        saliency_maps[i, coords[j][0], coords[j][1]] = WERj
+
+
+#%% ---------------------------------------------------------------------------
+
+
+from scipy.ndimage.filters import gaussian_filter
+
+
+smaps_mean = np.zeros((224,224), dtype=np.float32)
+smaps_variance = np.zeros((224,224), dtype=np.float32)
+smaps_smoothed = np.zeros(saliency_maps.shape, dtype=np.float32)
+
+print('Computing mean saliency')
+for i in range(nb_samples):
+    smaps_mean += saliency_maps[i]
+    
+smaps_mean = smaps_mean / nb_samples
+
+print('Computing saliency variance')
+for i in range(nb_samples):
+    smaps_variance += np.power(saliency_maps[i] - smaps_mean, 2)
+    
+smaps_variance = smaps_variance / nb_samples
+
+print('Computing smoothed saliency for visualization purposes')
+for i in range(nb_samples):
+    smaps_smoothed[i] = gaussian_filter(saliency_maps[i], 7)
+
+info = 'info, saliency_maps, smaps_smoothed, smaps_mean, smaps_variance'
+torch.save((info, saliency_maps, smaps_smoothed, smaps_mean, smaps_variance), 'saliency_maps.pth')
